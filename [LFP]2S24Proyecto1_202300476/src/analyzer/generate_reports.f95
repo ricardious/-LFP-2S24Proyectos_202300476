d366170a04c20ba3fb9d6c219854f51bbde30e84
ricardious <castaneda.systems@gmail.com>
2025-03-22 23:01:30 -0600

    chore(generate_reports.f95): add initial module structure

---
+++ [LFP]2S24Proyecto1_202300476/src/analyzer/generate_reports.f95
@@ -0,0 +1,11 @@
+module generate_report
+    use analyzerModule
+    implicit none
+
+contains
+
+    subroutine create_report(data, filename)
+        
+    end subroutine create_report
+
+end module generate_report
